05d2ed762f808f72c3660dab40f98ef5071b8789
Jaime Malvido <32091018+jmalvido@users.noreply.github.com>
2020-01-07 12:23:17 +0100

    Database con parser

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,6 +3,23 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
+		/*if (isset($_ENV["CLEARDB_DATABASE_URL"]))
+		{
+			$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+			$var_server = $cleardb_url["host"];
+			$var_username = $cleardb_url["pass"];
+			$var_db = substr($cleardb_url["path"], 1)
+
+
+		}
+		else //Entorno de desarollo
+		{
+		$var_server = "localhost";
+		$var_username = "username";
+		$var_password = "userpassword";
+		$var_db = "newdb";
+		}
+		*/
 		$this->user="bfbf2cd0b5e347";$this->pass="bd5fef2a";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_df1d6fa37af6ec9";
 	}
 
