955982992febce3e6230a948e1ff05859c4cf0e0
Jaime Malvido <32091018+jmalvido@users.noreply.github.com>
2022-01-20 09:30:30 +0100

    Update autoload.php

--- core/app/autoload.php
+++ core/app/autoload.php
@@ -2,18 +2,12 @@
 // autoload.php
 // 10 octubre del 2014
 // esta funcion elimina el hecho de estar agregando los modelos manualmente
-function __autoload($modelname){
+function autocargar($modelname){
 	if(Model::exists($modelname)){
 		include Model::getFullPath($modelname);
 	} 
 }
-
-/*function autocargar($modelname){
-	if(Model::exists($modelname)){
-		include Model::getFullPath($modelname);
-	} 
-}
-spl_autoload_register('autocargar');*/
+spl_autoload_register('autocargar');
 
 
 
